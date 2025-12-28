781825848b46361a42b591842eedd4f325502a1a
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 18:22:35 +0530

    Create Question 4.py

---
+++ Question 4.py
@@ -0,0 +1,13 @@
+'''
+Get Words That Come After "the"
+Write a function to find all the words that immediately follow the word "the" in a given sentence. The search should be case-insensitive, and words are separated by spaces.
+
+NOTE: This is a function type question, you don't have to take input or print the output, just have to complete the required function definition.
+
+Examples
+
+"The key and the lock is there" -> ['key', 'lock']
+"The" (case-insensitive) is followed by "key" and "the" is followed by "lock" hence, the output is ['key', 'lock'].
+"The the and the The" -> ['the', 'and', 'The']
+"the" follows "The", "and" follows "the" and "The" follows "the".
+'''
