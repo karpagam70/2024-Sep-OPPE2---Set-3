7b6026d93d8b43f71b3abd4cb83dcf932364fe20
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 18:23:14 +0530

    Create Question 5.py

---
+++ Question 5.py
@@ -0,0 +1,20 @@
+'''
+Ceil Marks to Nearest Tens if Close
+Write a program to process roll numbers and marks for students. If a student's marks have a unit digit of 8 or 9, round their marks up to the nearest tens. Otherwise, keep the marks unchanged. Print only the roll number and updated marks for students whose marks were updated.
+
+NOTE: This is an I/O type question, you need to write the whole code for taking input and printing the output.
+
+Input Format
+
+The first line contains the number of lines of student data n.
+Next n lines contains the student data with roll number and marks separated by space.
+Output Format
+
+Student data for the roll number whose marks has been updated in the format of roll number and separated by comma.
+Examples
+
+48 -> 50
+69 -> 70
+70 -> 70
+37 -> 37
+'''
