ac9927b14b64d29af5b6b2ad3701656d18dd6577
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 18:19:48 +0530

    Create Question 1.py

---
+++ Question 1.py
@@ -0,0 +1,28 @@
+'''
+Product of Sum and Absolute Difference of Digits in a Two-Digit Number
+Given a two-digit number, calculate the product of:
+
+The sum of its digits.
+The absolute difference between its digits.
+Assume the input is a two-digit number.
+
+NOTE: This is a function type question, you don't have to take input or print the output, just have to complete the required function definition.
+
+Input: A two-digit integer.
+Output: An integer representing the product of the sum and absolute difference of its digits.
+
+Example
+
+Input
+
+54
+Output
+
+9
+Explanation
+
+Digits are 5 and 4.
+Sum = 5 + 4 = 9.
+Absolute Difference = |5 - 4| = 1.
+Product = 9 * 1 = 9.
+'''
